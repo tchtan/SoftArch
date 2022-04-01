2f6e41d095d8741815961d84ce4c736e46f9714d
tchtan <tctan.ch@gmail.com>
2022-04-01 19:01:37 +0700

    final

---
+++ Structural Design Patterns/TaxPayingHealthcareWorker.java
@@ -0,0 +1,13 @@
+package edu.parinya.softarchdesign.structural;
+
+public class TaxPayingHealthcareWorker extends HealthcareWorkerDecorator {
+    public TaxPayingHealthcareWorker(HealthcareWorker worker) {
+        super(worker);
+        System.out.println("Decorate " + worker.getName() + " with TaxPaying.");
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.1f * super.getPrice();
+    }
+}
